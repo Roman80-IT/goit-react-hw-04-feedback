ada6abbff339a8bfa46f1decd338ffbaf5f86bf7
Roman <jrj80mail@gmail.com>
2023-10-16 23:59:54 +0300

    se

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -7,7 +7,13 @@ import { Notification } from './Notification/Notification';
 
 import { Wrapper } from './App.styled';
 
+// export class App extends Component {
 export const App = () => {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
   const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
@@ -16,6 +22,9 @@ export const App = () => {
 
   // Метод збільшення відгуків (одним методом)
   const incrementFeedback = option => {
+    // this.setState(prevState => ({
+    //   [option]: prevState[option] + 1,
+    // }));
     setFeedback(prevState => ({
       ...prevState,
       [option]: prevState[option] + 1,
@@ -23,15 +32,20 @@ export const App = () => {
   };
 
   const countTotalFeedback = () => {
+    // const { good, neutral, bad } = this.state;
+    // return good + neutral + bad;
     return feedback.good + feedback.neutral + feedback.bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
+    // const { good } = this.state;
     const { good } = feedback;
     const total = countTotalFeedback();
     return total === 0 ? 0 : (good / total) * 100;
   };
 
+  // render() {
+  // const { good, neutral, bad } = this.state;
   const totalFeedback = countTotalFeedback();
   const positivePercentage = countPositiveFeedbackPercentage();
   const options = Object.keys(feedback);
